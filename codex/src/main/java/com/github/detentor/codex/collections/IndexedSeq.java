c38edcbc9d1422e26e0e4d6fed2b0c62946b7512
f9540702 <f9540702@esbsa019946es06.df.intrabb.bb.com.br>
2012-12-07 16:02:43 -0200

    Criação de nova interface.

--- codex/src/main/java/com/github/detentor/codex/collections/IndexedSeq.java
+++ codex/src/main/java/com/github/detentor/codex/collections/IndexedSeq.java
@@ -4,7 +4,7 @@ import com.github.detentor.codex.function.PartialFunction;
 
 /**
  * Sequências indexadas são sequências que garantem tempo constante no acesso 
- * a elementos em posição aleatória ou ao seu tamanho. <br/><br/>
+ * a elementos em posição aleatória e ao seu tamanho. <br/><br/>
  * 
  * Além disso, possuem capacidade de extrair rapidamente sub-sequências (tempo, no máximo, proporcional ao tamanho
  * da sub-sequência).
@@ -12,22 +12,22 @@ import com.github.detentor.codex.function.PartialFunction;
  * @author Vinícius Seufitele Pinto
  *
  */
-public interface IndexedSeq<A> extends PartialFunction<Integer, A>
+public interface IndexedSeq<A> extends PartialFunction<Integer, A>, SharpCollection<A>
 {
 	/**
-	 * Retorna o número de elementos desta coleção.
-     *
+	 * Retorna o número de elementos desta coleção. <br/>
+	 * Esta operação tem garantia de ser executada em tempo constante.
      * @return O número de elementos desta coleção
      */
     int size();
-    
+
     /**
-     * Retorna uma sub-sequência desta sequência, a partir 
-     * @param startIndex
-     * @param endIndex
-     * @return
+     * Retorna uma sub-sequência desta sequência, a partir do índice de início e fim passados como parâmetro
+     * @param startIndex O índice onde a sub-sequência será iniciada
+     * @param endIndex O índice onde a sub-sequência terminará
+     * @return A sub-sequência desta sequência que representa o intervalo determinado
      */
-//    IndexedSeq<A> subsequence(int startIndex, int endIndex);
-	
+    IndexedSeq<A> subsequence(int startIndex, int endIndex);
+
 
 }
