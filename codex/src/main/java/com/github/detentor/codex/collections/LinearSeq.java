799d60902e257079195a86322212993e3a976057
f9540702 <f9540702@esbsa019946es06.df.intrabb.bb.com.br>
2012-12-07 16:02:58 -0200

    Criação de nova interface.

---
+++ codex/src/main/java/com/github/detentor/codex/collections/LinearSeq.java
@@ -0,0 +1,36 @@
+package com.github.detentor.codex.collections;
+
+import java.util.Iterator;
+
+import com.github.detentor.codex.function.PartialFunction;
+
+/**
+ * Sequências lineares representam sequências onde, assim como {@link Iterator iterators}, 
+ * trata-se um elemento de cada vez.<br/><br/>
+ * 
+ * Sequências lineares garantem tempo constante para os métodos
+ * {@link #head()}, {@link #tail()}, {@link #isEmpty()}.
+ * 
+ * @author Vinícius Seufitele Pinto
+ *
+ */
+public interface LinearSeq<A> extends PartialFunction<Integer, A>, SharpCollection<A>
+{
+	 /**
+	 * {@inheritDoc}
+	 * <br/>Esta operação tem garantia de ser executada em tempo constante.
+	 */
+    boolean isEmpty();
+
+    /**
+     * {@inheritDoc}
+	 * <br/>Esta operação tem garantia de ser executada em tempo constante.
+	 */
+    IndexedSeq<A> tail();
+    
+    /**
+     * {@inheritDoc}
+     * <br/>Esta operação tem garantia de ser executada em tempo constante.
+     */
+    A head();
+}
