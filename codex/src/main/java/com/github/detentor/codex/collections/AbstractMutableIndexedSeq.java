5783a71ea583e7246cb9bfbd1c4e7d68811d7896
f9540702 <f9540702@esbsa019946es06.df.intrabb.bb.com.br>
2012-12-10 15:20:30 -0200

    Remodelagem da coleção.

---
+++ codex/src/main/java/com/github/detentor/codex/collections/AbstractMutableIndexedSeq.java
@@ -0,0 +1,54 @@
+package com.github.detentor.codex.collections;
+
+import com.github.detentor.codex.function.Function1;
+import com.github.detentor.codex.function.PartialFunction;
+
+/**
+ * Classe que provê a implementação padrão de métodos mutáveis de coleções indexadas sequencialmente, para simplificar a 
+ * criação de classes que os estenda. <br/> <br/>
+ * 
+ * Para criar uma coleção (mutável) com base nesta implementação, basta prover o código dos seguintes métodos: <br/> <br/>
+ * 
+ * {@link Iterable#iterator() iterator()} , {@link SharpCollection#size() size()} , 
+ * {@link #subsequence(int, int)} , 
+ * {@link #apply(Integer)} ,
+ * {@link SharpCollection#builder() builder()} <br/> <br/>
+ * 
+ * Não esquecer de sobrescrever o equals e o hashcode também. <br/><br/>
+ * 
+ * Para coleções imutáveis, veja {@link AbstractIndexedSeq}. <br/><br/>
+ * 
+ * NOTA: Subclasses devem sempre dar override nos métodos {@link #map(Function1) map}, {@link #collect(PartialFunction) collect} 
+ * e {@link #flatMap(Function1) flatMap}. Devido à incompetência do Java com relação a Generics,
+ * isso é necessário para assegurar que o tipo de retorno seja o mesmo da coleção. A implementação padrão (chamado o método da super
+ * classe é suficiente).
+ * 
+ * @author Vinícius Seufitele Pinto
+ *
+ */
+public abstract class AbstractMutableIndexedSeq<T, U extends IndexedSeq<T>> extends AbstractIndexedSeq<T> 
+												   implements IndexedSeq<T>, MutableSharpCollection<T>
+{
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public U addAll(final Iterable<? extends T> col)
+	{
+		for (final T ele : col)
+		{
+			this.add(ele);
+		}
+		return (U) this;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public U removeAll(final Iterable<T> col)
+	{
+		for (final T ele : col)
+		{
+			this.remove(ele);
+		}
+		return (U) this;
+	}
+}
