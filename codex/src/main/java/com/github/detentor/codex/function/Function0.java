cc57f0bd151100af322e07fdee343f00c5552a15
f9540702 <f9540702@esbsa019946es06.df.intrabb.bb.com.br>
2013-01-21 08:30:48 -0200

    Criação do functionN e melhoria da documentação da function0.

--- codex/src/main/java/com/github/detentor/codex/function/Function0.java
+++ codex/src/main/java/com/github/detentor/codex/function/Function0.java
@@ -1,17 +1,17 @@
 package com.github.detentor.codex.function;
 
 /**
- * Interface que define uma função que não possui parâmetros, e retorna um valor do tipo T
+ * Interface que define uma função que não possui parâmetros, e retorna um valor do tipo A
  * @author f9540702 Vinícius Seufitele Pinto
  *
- * @param <T> O tipo a ser retornado pela função
+ * @param <A> O tipo a ser retornado pela função
  */
-public interface Function0<T>
+public interface Function0<A>
 {
 	/**
-	 * Executa a função, retornando um valor do tipo T
-	 * @return Um valor do tipo T
+	 * Executa a função, retornando um valor do tipo A
+	 * @return Um valor do tipo A
 	 */
-	T apply();
+	A apply();
 
 }
---
+++ codex/src/main/java/com/github/detentor/codex/function/FunctionN.java
@@ -0,0 +1,20 @@
+package com.github.detentor.codex.function;
+
+/**
+ * Interface que define uma função que não recebe uma quantidade arbitrária de argumentos
+ * do tipo A, e retorna um valor do tipo B
+ * 
+ * @author f9540702 Vinícius Seufitele Pinto
+ *
+ * @param <A> O tipo dos parâmetros da função
+ * @param <B> O tipo de retorno da função
+ */
+public interface FunctionN<A, B>
+{
+	/**
+	 * Executa a função, a partir da aplicação dos parâmetros passados.
+	 * @param params Os parâmetros a serem passados para a função
+	 * @return Um valor do tipo B, obtido após a aplicação da função para os parâmetros
+	 */
+	B apply(final A... params);
+}
