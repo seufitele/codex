dcb74dad7a509766308c12cb513fd08641ffbd50
detentor <vinicius.seufitele@gmail.com>
2013-01-21 21:21:21 -0200

    Início de uma nova estrutura de dados.

---
+++ codex/src/main/java/com/github/detentor/codex/collections/mutable/Vector.java
@@ -0,0 +1,78 @@
+package com.github.detentor.codex.collections.mutable;
+
+import java.util.Iterator;
+
+import com.github.detentor.codex.collections.AbstractMutableIndexedSeq;
+import com.github.detentor.codex.collections.Builder;
+import com.github.detentor.codex.collections.IndexedSeq;
+import com.github.detentor.codex.collections.SharpCollection;
+
+/**
+ * 
+ * @author Vinicius Seufitele
+ *
+ * @param <T>
+ */
+public class Vector<T> extends AbstractMutableIndexedSeq<T, Vector<T>>
+{
+	final Object[] data = new Object[32]; // 
+	
+
+	@Override
+	public T apply(Integer pos)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public IndexedSeq<T> subsequence(int startIndex, int endIndex)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterator<T> iterator()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <B> Builder<B, SharpCollection<B>> builder()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SharpCollection<T> add(T element)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SharpCollection<T> remove(T element)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SharpCollection<T> clear()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
