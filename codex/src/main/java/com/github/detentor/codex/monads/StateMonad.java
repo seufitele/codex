3b9295288620b2ec5cce4df4f26b6160b36bc6fc
f9540702 <f9540702@esbsa019946es06.df.intrabb.bb.com.br>
2013-01-22 09:19:30 -0200

    Melhoria na documentação.

--- codex/src/main/java/com/github/detentor/codex/monads/StateMonad.java
+++ codex/src/main/java/com/github/detentor/codex/monads/StateMonad.java
@@ -1,20 +1,20 @@
 package com.github.detentor.codex.monads;
 
 
-/**
- * State Monad é uma mônade que carrega um estado do tipo A.
- * 
- * @author f9540702
- *
- * @param <A>
- * @param <B>
- * @param <C>
- */
-public abstract class StateMonad<A, B> implements Monad<B>
-{
-	protected final A state;
 
-	protected StateMonad(final A theState)
+/**
+ * State Monad é uma mônade para um tipo A que carrega um estado do tipo S.
+ * 
+ * @author Vinicius Seufitele Pinto
+ *
+ * @param <S> O tipo de dados do estado da mônade
+ * @param <A> O tipo de dados guardado pela mônade
+ */
+public abstract class StateMonad<S, A> implements Monad<A>
+{
+	protected final S state;
+
+	protected StateMonad(final S theState)
 	{
 		super();
 		this.state = theState;
