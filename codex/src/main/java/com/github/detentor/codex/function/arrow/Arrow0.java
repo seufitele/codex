a6c836a7772d3a98ffc2203f78ca09e9ad05203b
f9540702 <f9540702@esbsa019946es06.df.intrabb.bb.com.br>
2013-01-24 11:48:43 -0200

    Melhoria na documentação.

--- codex/src/main/java/com/github/detentor/codex/function/arrow/Arrow0.java
+++ codex/src/main/java/com/github/detentor/codex/function/arrow/Arrow0.java
@@ -2,6 +2,13 @@ package com.github.detentor.codex.function.arrow;
 
 import com.github.detentor.codex.function.Function0;
 
+/**
+ * Essa classe representa uma seta que não recebe parâmetros (uma seta constante).
+ * 
+ * @author Vinicius Seufitele Pinto
+ *
+ * @param <A>
+ */
 public abstract class Arrow0<A> implements Function0<A>, Arrow
 {
 	@Override
@@ -9,4 +16,5 @@ public abstract class Arrow0<A> implements Function0<A>, Arrow
 	{
 		return 0;
 	}
+
 }
