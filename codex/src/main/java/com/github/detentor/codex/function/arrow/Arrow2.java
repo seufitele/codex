3f3d798c27df3db435978d1bb92f53dbfcb70f2f
f9540702 <f9540702@esbsa019946es06.df.intrabb.bb.com.br>
2013-03-26 14:35:37 -0300

    correção de documentação

--- codex/src/main/java/com/github/detentor/codex/function/arrow/Arrow2.java
+++ codex/src/main/java/com/github/detentor/codex/function/arrow/Arrow2.java
@@ -4,7 +4,13 @@ import com.github.detentor.codex.function.Function2;
 
 public abstract class Arrow2<A, B, C> implements Function2<A, B, C>, Arrow
 {
-	
+	/**
+	 * Retorna uma seta de aridade 1, a partir da aplicação do primeiro parâmetro. <br/>
+	 * Essa técnica é também conhecida como Currying.
+	 * 
+	 * @param param1 O parâmetro a ser aplicado parcialmente nesta seta
+	 * @return Uma seta de aridade 1, que representa esta seta com o parâmetro passado fixado
+	 */
 	public Arrow1<B, C> applyPartial(final A param1)
 	{
 		return new Arrow1<B, C>()
