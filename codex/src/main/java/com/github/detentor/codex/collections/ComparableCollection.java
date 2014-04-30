2f1676730af4a65bca807e10ee1918d2e8e581dc
detentor <vinicius.seufitele@gmail.com>
2014-04-30 10:22:32 -0300

    Codificação de métodos.

--- codex/src/main/java/com/github/detentor/codex/collections/ComparableCollection.java
+++ codex/src/main/java/com/github/detentor/codex/collections/ComparableCollection.java
@@ -1,7 +1,5 @@
 package com.github.detentor.codex.collections;
 
-import java.util.Comparator;
-import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 import com.github.detentor.codex.monads.Option;
@@ -12,7 +10,7 @@ import com.github.detentor.codex.monads.Option;
  * @author Vinícius Seufitele Pinto
  *
  */
-public interface ComparableCollection<T extends Comparable<? super T>> extends SharpCollection<T>
+public interface ComparableCollection<T extends Comparable<? super T>> extends Seq<T>
 {
 	/**
 	 * Retorna o valor mínimo desta coleção.
@@ -21,63 +19,16 @@ public interface ComparableCollection<T extends Comparable<? super T>> extends S
 	 * @return O elemento com o menor valor na comparação
 	 * @throws NoSuchElementException Caso a coleção esteja vazia
 	 */
-//	 *@throws ClassCastException Caso os elementos desta classe não sejam do tipo Comparator <- NÃO DEVERIA ACONTECER MAIS!!!
 	default T min()
 	{
-		if (this.isEmpty())
-		{
-			throw new NoSuchElementException("min foi chamado para uma coleção vazia");
-		}
-
-		final Iterator<T> ite = this.iterator();
-		T minValue = ite.next();
-
-		while (ite.hasNext())
-		{
-			final T curEle = ite.next();
-			if (curEle.compareTo(minValue) < 0)
-			{
-				minValue = curEle;
-			}
-		}
-		return minValue;
+		return min((param1, param2) -> param1.compareTo(param2));
 	}
-	
-	/**
-	 * Retorna o valor mínimo, a partir da função de comparação passada como parâmetro. <br/>
-	 * Formalmente, retorna um valor T tal que não exista um elemento U onde comparator.compare(T, U) < 0. <br/>
-	 * Havendo mais de um valor T com essa propriedade, o primeiro deles é retornado. 
-	 * @param comparator A função de comparação entre os elementos.
-	 * @return O elemento com o menor valor na comparação
-	 * @throws NoSuchElementException Caso a coleção esteja vazia
-	 */
-	default T min(final Comparator<? super T> comparator)
-	{
-		if (this.isEmpty())
-		{
-			throw new NoSuchElementException("min foi chamado para uma coleção vazia");
-		}
 
-		final Iterator<T> ite = this.iterator();
-		T minValue = ite.next();
-
-		while (ite.hasNext())
-		{
-			final T curEle = ite.next();
-			if (comparator.compare(curEle, minValue) < 0)
-			{
-				minValue = curEle;
-			}
-		}
-		return minValue;
-	}
-	
 	/**
 	 * Retorna uma Option contendo o valor retornado por {@link #min()}, se esta coleção possuir
 	 * elementos, ou uma Option vazia, se esta coleção estiver vazia.
 	 * @return Uma Option que conterá o valor mínimo, caso esta coleção não esteja vazia
 	 */
-//	 * @throws ClassCastException Caso os elementos desta classe não sejam do tipo Comparator
 	default Option<T> minOption()
 	{
 		return this.isEmpty() ? Option.<T>empty() : Option.from(min());
@@ -90,63 +41,16 @@ public interface ComparableCollection<T extends Comparable<? super T>> extends S
 	 * @return O elemento com o maior valor na comparação
 	 * @throws NoSuchElementException Caso a coleção esteja vazia
 	 */
-//	 * @throws ClassCastException Caso os elementos desta classe não sejam do tipo Comparator
 	default T max()
 	{
-		if (this.isEmpty())
-		{
-			throw new NoSuchElementException("max foi chamado para uma coleção vazia");
-		}
-
-		final Iterator<T> ite = this.iterator();
-		T maxValue = ite.next();
-
-		while (ite.hasNext())
-		{
-			final T curEle = ite.next();
-			if (curEle.compareTo(maxValue) > 0)
-			{
-				maxValue = curEle;
-			}
-		}
-		return maxValue;
+		return min((param1, param2) -> param2.compareTo(param1));
 	}
-	
-	/**
-	 * Retorna o valor máximo, a partir da função de comparação passada como parâmetro. <br/>
-	 * Formalmente, retorna um valor T tal que não exista um elemento U onde a comparator.compare(T, U) > 0. <br/>
-	 * Havendo mais de um valor T com essa propriedade, o primeiro deles é retornado. 
-	 * @param comparator A função de comparação entre os elementos.
-	 * @return O elemento com o maior valor na comparação
-	 * @throws NoSuchElementException Caso a coleção esteja vazia
-	 */
-	default T max(final Comparator<? super T> comparator)
-	{
-		if (this.isEmpty())
-		{
-			throw new NoSuchElementException("max foi chamado para uma coleção vazia");
-		}
 
-		final Iterator<T> ite = this.iterator();
-		T maxValue = ite.next();
-
-		while (ite.hasNext())
-		{
-			final T curEle = ite.next();
-			if (comparator.compare(curEle, maxValue) > 0)
-			{
-				maxValue = curEle;
-			}
-		}
-		return maxValue;
-	}
-	
 	/**
 	 * Retorna uma Option contendo o valor retornado por {@link #max()}, se esta coleção possuir
 	 * elementos, ou uma Option vazia, se esta coleção estiver vazia.
 	 * @return Uma Option que conterá o valor máximo, caso esta coleção não esteja vazia
 	 */
-//	 * @throws ClassCastException Caso os elementos desta classe não sejam do tipo Comparator
 	default Option<T> maxOption()
 	{
 		return this.isEmpty() ? Option.<T>empty() : Option.from(max());
@@ -157,9 +61,11 @@ public interface ComparableCollection<T extends Comparable<? super T>> extends S
 	 * 
 	 * @return A coleção com os elementos ordenados, se for mutável, ou uma nova coleção, se imutável.
 	 */
-	SharpCollection<T> sorted();
-	
-	
+	default Seq<T> sorted()
+	{
+		return sorted((param1, param2) -> param1.compareTo(param2));
+	}
+
 //	/**
 //	 * Retorna o valor mínimo, a partir da função de mapeamento passada como parâmetro. <br/>
 //	 * @param func
