1e67e7e255f7418254b4404ca57a12cab643b4c2
detentor <vinicius.seufitele@gmail.com>
2015-10-07 09:03:06 -0300

    Criação da composição para ArrowN

--- codex/src/main/java/com/github/detentor/codex/function/arrow/ArrowN.java
+++ codex/src/main/java/com/github/detentor/codex/function/arrow/ArrowN.java
@@ -2,6 +2,7 @@ package com.github.detentor.codex.function.arrow;
 
 import java.lang.reflect.Array;
 
+import com.github.detentor.codex.function.Function1;
 import com.github.detentor.codex.function.FunctionN;
 
 /**
@@ -41,5 +42,30 @@ public abstract class ArrowN<A, B> implements FunctionN<A, B>, Arrow
 	{
 		return 0;
 	}
-
+	
+	 /**
+     * Faz a composição desta seta com a seta passada como parâmetro. <br/>
+     * Formalmente, seja
+     * 
+     * <pre>
+     * f: A -> B e g: B -> C. Então, será retornado um h: A -> C.
+     * </pre>
+     * 
+     * @param <A> O tipo de dados de entrada da primeira seta
+     * @param <B> O tipo de dados de retorno da primeira seta, e de entrada da segunda (contravariante)
+     * @param <C> O tipo de dados de retorno da segunda seta
+     * @param func Uma função <b>g: B -> C</b>, a ser feita a composição.
+     * @return Uma seta <b>h: A -> C</b>, que representa a composição das duas setas.
+     */
+    public <C> ArrowN<A, C> and(final Function1<? super B, C> func)
+    {
+        return new ArrowN<A, C>()
+        {
+            @Override
+            public C apply(A... params)
+            {
+                return func.apply(ArrowN.this.apply(params));
+            }
+        };
+    }
 }
